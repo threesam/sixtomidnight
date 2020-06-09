f63aa84514df86939d161d15f7006dc462b5eb57
salvatoredangelo <57677512+salvatoredangelo@users.noreply.github.com>
2020-06-09 15:03:46 -0400

    Initial commit

---
+++ studio/README.md
@@ -0,0 +1,1 @@
+# sixtomidnight-studio
---
+++ studio/dashboardConfig.js
@@ -0,0 +1,53 @@
+export default {
+  widgets: [
+    {
+      name: 'sanity-tutorials',
+      options: {
+        templateRepoId: 'sanity-io/sanity-template-gatsby-blog'
+      }
+    },
+    {name: 'structure-menu'},
+    {
+      name: 'project-info',
+      options: {
+        __experimental_before: [
+          {
+            name: 'netlify',
+            options: {
+              description:
+                'NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.',
+              sites: [
+                {
+                  buildHookId: '5edfdd0d4858740277f1dc6f',
+                  title: 'Sanity Studio',
+                  name: 'sixtomidnight-studio',
+                  apiId: 'f4acbe95-1c75-4791-871e-8147a8813bbb'
+                },
+                {
+                  buildHookId: '5edfdd0dfe49360264959c41',
+                  title: 'Blog Website',
+                  name: 'sixtomidnight',
+                  apiId: '4897a6b5-2760-4066-8e79-725883f3f42b'
+                }
+              ]
+            }
+          }
+        ],
+        data: [
+          {
+            title: 'GitHub repo',
+            value: 'https://github.com/salvatoredangelo/sixtomidnight',
+            category: 'Code'
+          },
+          {title: 'Frontend', value: 'https://sixtomidnight.netlify.app', category: 'apps'}
+        ]
+      }
+    },
+    {name: 'project-users', layout: {height: 'auto'}},
+    {
+      name: 'document-list',
+      options: {title: 'Recent blog posts', order: '_createdAt desc', types: ['post']},
+      layout: {width: 'medium'}
+    }
+  ]
+}
