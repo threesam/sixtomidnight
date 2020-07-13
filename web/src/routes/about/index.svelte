2d4fe177dea7ac2012bcfb41267282de669fe46b
salvatoredangelo <salvatoredangelo@protonmail.com>
2020-07-12 22:39:40 -0400

    added team and teamMembers and some basic, and absolutely temporary, centering because why not

--- web/src/routes/about/index.svelte
+++ web/src/routes/about/index.svelte
@@ -1,7 +1,45 @@
+<script context="module">
+  import client from '../../sanityClient'
+  export async function preload({ params, query }) {
+    const team = await client.fetch(
+      '*[_type == "author"]{name, "image": image.asset->url, "alt": image.alt, "caption": image.caption, "slug": slug.current}',
+    )
+    console.log(team)
+    return { team }
+  }
+</script>
+
+<script>
+  import TeamMember from '../../components/TeamMember.svelte'
+  export let team
+</script>
+
+<style>
+  section {
+    text-align: center;
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+    align-items: center;
+    margin: 1rem;
+  }
+  div {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    grid-gap: 2rem;
+  }
+</style>
+
 <svelte:head>
   <title>About</title>
 </svelte:head>
 
-<h1>About this site</h1>
+<section>
+  <h1>Team</h1>
 
-<p>This is the 'about' page. There's not much here.</p>
+  <div>
+    {#each team as teamMember}
+      <TeamMember {teamMember} />
+    {/each}
+  </div>
+</section>
