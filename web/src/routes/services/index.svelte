6673244133d9cb79223530701b509a8c58ed4eec
threesam <salvatoredangelo@protonmail.com>
2020-11-18 14:29:28 -0500

    queried for services in services

--- web/src/routes/services/index.svelte
+++ web/src/routes/services/index.svelte
@@ -1,1 +1,31 @@
+<script context="module">
+  import client from '../../sanityClient'
+  export function preload({ params, query }) {
+    return client
+      .fetch(
+        '*[_type == "service" && defined(slug.current) && publishedAt < now()]|order(publishedAt desc)',
+      )
+      .then((services) => {
+        return { services }
+      })
+      .catch((err) => this.error(500, err))
+  }
+</script>
+
+<script>
+  export let services
+</script>
+
 <h1>Services</h1>
+
+{#if !services.length}
+  <p>seems like we don't have any</p>
+{:else}
+<ul>
+
+  {#each services as service}
+  <!-- content here -->
+  <li>{service.title}</li>
+  {/each}
+</ul>
+{/if}
