220f6bbcb4b8df488ac537ff9612aeb103c814e0
threesam <salvatoredangelo@protonmail.com>
2020-11-17 22:20:24 -0500

    custom studio logo, updated dependencies

---
+++ studio/logo/myLogo.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './myLogo.css'
+
+const MyLogo = () => (
+  <img className={styles.myLogo} src="static/logo.jpg" alt="six to midnight logo" />
+)
+
+export default MyLogo
