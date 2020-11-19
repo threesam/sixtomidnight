73f0455842fc817a1d2a2a6015037ab168c97bed
threesam <salvatoredangelo@protonmail.com>
2020-11-19 16:16:20 -0500

    remvoed AboutUs component, and pulled in Sanity page data

--- web/src/routes/about/index.svelte
+++ web/src/routes/about/index.svelte
@@ -1,21 +1,58 @@
+<script context="module">
+  import client from '../../sanityClient'
+  import BlockContent from "@movingbrands/svelte-portable-text";
+  import serializers from "../../components/serializers";
+  export async function preload({ params, query }) {
+    const about = await client.fetch(
+      `*[_type == "page" && title == "About Us"][0]{
+        title,
+        "image": mainImage.asset->url,
+        "alt": mainImage.alt,
+        body[]{
+          ...,
+          children[]{
+            ...
+          }
+        }
+      }`,
+    )
+    return { about }
+  }
+</script>
+
 <script>
-  import AboutUs from './AboutUs.svelte'
+    import { fly } from 'svelte/transition'
+
+  export let about
+  const {title, image, alt, body} = about
+  console.log(about)
 </script>
 
 <style>
-  section {
+  article {
     display: flex;
     flex-flow: column;
     justify-content: center;
   }
+  img {
+    width: 100%;
+    height: 300px;
+    object-fit: cover;
+  }
 </style>
 
 <svelte:head>
-  <title>About</title>
+  <title>{title}</title>
 </svelte:head>
 
-<section>
-  <AboutUs />
-</section>
-<a href="/about/tribute">tribute</a>
-<a href="/about/team">team</a>
+<article>
+  <section>
+    <h1>{title}</h1>
+    <img src="{image}" {alt} />
+    <div in:fly={{ y: -100 }}>
+      <BlockContent blocks={body} {serializers} />
+    </div>
+    <a href="/about/tribute">tribute</a>
+    <a href="/about/team">team</a>
+  </section>
+</article>
